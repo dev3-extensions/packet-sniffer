4183017809e4b7216ad4eebc490b6bab726d088a
Aryan Prince <aryanprincesanty@icloud.com>
2023-04-29 23:00:57 +0100

    feat: Built user interface and now displays HTTP packet info (#1)
    * chore: Updated permissions
    * chore: Added new dependencies
    * chore: Added CSS for dark scrollbars
    * feat: Added new components to build frontend
    * feat: Built working user interface
    * feat: Added checkboxes UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,91 @@
-function App() {
+import { useEffect, useState } from 'react'
+
+import { TableDemoData } from '~/components/TableDemoData'
+import { TableHeader } from '~/components/TableHeader'
+
+import { ClearButton } from '~/components/ui/ClearButton'
+import { PauseButton } from '~/components/ui/PauseButton'
+import { SaveButton } from '~/components/ui/SaveButton'
+
+export default function App() {
+  const [httpRequests, setHttpRequests] = useState<chrome.webRequest.WebResponseCacheDetails[]>([])
+  const [isPaused, setIsPaused] = useState(false)
+
+  useEffect(() => {
+    if (!isPaused) {
+      const handleRequest = (details: chrome.webRequest.WebResponseCacheDetails) => {
+        const request: chrome.webRequest.WebResponseCacheDetails = {
+          url: details.url,
+          method: details.method,
+          statusCode: details.statusCode,
+          statusLine: details.statusLine,
+          frameId: details.frameId,
+          fromCache: details.fromCache,
+          parentFrameId: details.parentFrameId,
+          requestId: details.requestId,
+          tabId: details.tabId,
+          timeStamp: details.timeStamp,
+          type: details.type,
+        }
+        setHttpRequests((prevRequests) => [...prevRequests, request])
+      }
+
+      chrome.webRequest.onCompleted.addListener(handleRequest, { urls: ['<all_urls>'] })
+
+      return () => {
+        chrome.webRequest.onCompleted.removeListener(handleRequest)
+      }
+    }
+  }, [])
+
   return (
-    <>
-      <div className="min-h-[400px] w-[600px] bg-[#262626] text-white">
-        <div className="m-4 flex flex-col gap-4">
-          <p className="text-2xl font-semibold">Packet Sniffer</p>
-          {/* Additional code goes here */}
+    <div className="h-[600px] w-[800px] overflow-y-scroll bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-white">
+      <div className="p-4 flex flex-col gap-6">
+        <div className="flex gap-4 items-center">
+          <p className="text-3xl font-bold">Packet Sniffer</p>
+          <ClearButton setHttpRequests={setHttpRequests} />
+          <PauseButton setIsPaused={setIsPaused} />
+          <SaveButton />
+        </div>
+        <div>
+          <table className="w-full border-collapse border border-neutral-600 table-fixed text-sm text-left text-neutral-500 dark:text-neutral-400">
+            <thead className="text-xs text-neutral-700 uppercase bg-neutral-50 dark:bg-neutral-700 dark:text-neutral-400">
+              <TableHeader />
+            </thead>
+            <tbody className="text-neutral-100 divide-y divide-neutral-700">
+              <TableDemoData />
+              {httpRequests.map((request, index) => (
+                <tr key={index} className="bg-white dark:bg-neutral-800">
+                  <td className="border border-neutral-500 text-center py-3 w-10">
+                    <div className="flex items-center justify-center">
+                      <input
+                        id="checkbox-all-search"
+                        type="checkbox"
+                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      ></input>
+                      <label htmlFor="checkbox-all-search" className="sr-only">
+                        checkbox
+                      </label>
+                    </div>
+                  </td>
+                  <td className="border border-neutral-600 py-2 text-center">{request.method}</td>
+                  <td className="border border-neutral-600 py-2 pl-2 truncate">{request.url}</td>
+                  <td className="border border-neutral-600 py-2 text-center">
+                    {request.statusCode}
+                  </td>
+                  <td className="border border-neutral-600 py-2 text-center truncate">
+                    {request.timeStamp}
+                  </td>
+                  <td className="border border-neutral-600 py-2 text-center">
+                    {request.fromCache}
+                  </td>
+                  <td className="border border-neutral-600 py-2 text-center">{request.ip}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
-    </>
+    </div>
   )
 }
-
-export default App
